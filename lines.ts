510efa812d6979ca378cffbee65529d861a7cb22
Mirek Rusin <mirek@me.com>
2020-10-28 09:20:01 +0100

    Adding lines.

---
+++ lines.ts
@@ -0,0 +1,14 @@
+import { inspect } from 'util'
+import auto from './auto'
+import raw from './raw'
+import type S from './sanitised'
+
+export const lines =
+  <T>(xs: readonly T[], separator: string): S => {
+    if (!Array.isArray(xs)) {
+      throw new TypeError(`Expected array, got ${inspect(xs)}.`)
+    }
+    return raw(xs.filter(_ => typeof _ !== undefined).map(_ => auto(_).toString().trim()).join(separator))
+  }
+
+export default lines
