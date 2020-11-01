89746c14b416c873999d1586332ade827c911a98
Mirek Rusin <mirek@me.com>
2020-11-01 16:13:59 +0100

    Updating interpolate, adding margin.

--- helpers/interpolate.ts
+++ helpers/interpolate.ts
@@ -1,9 +1,14 @@
 import { inspect } from 'util'
+import type S from '../sanitised'
+import marginOfLastLine from './margin-of-last-line'
+import indentTail from './indent-tail'
 
-/** @returns interpolated array `xs` with values from `ys`; empty array if both arrays are empty.
-    @throws if `xs.length - 1 !== ys.length` */
+/**
+ * @returns interpolated array `xs` with values from `ys`; empty array if both arrays are empty.
+ * @throws if `xs.length - 1 !== ys.length`
+ */
 export const interpolate =
-  <T, I>(xs: TemplateStringsArray | T[], ys: I[]): (string | T | I)[] => {
+  (xs: TemplateStringsArray, ys: S[]): string[] => {
     if (!Array.isArray(xs) || !Array.isArray(ys)) {
       throw new TypeError(`Expected xs and ys to be an array, got ${inspect(xs)} and ${inspect(ys)}.`)
     }
@@ -13,9 +18,10 @@ export const interpolate =
     if (xs.length - 1 !== ys.length) {
       throw new TypeError(`Expected xs and ys length to be correct, got ${inspect(xs.length)} and ${inspect(ys.length)}.`)
     }
-    const rs: (T | I | string)[] = [ xs[0] ]
+    const rs: string[] = [ xs[0] ]
     for (let i = 0; i < ys.length; i++) {
-      rs.push(ys[i])
+      const margin = marginOfLastLine(rs[rs.length - 1])
+      rs.push(indentTail(ys[i].toString(), margin))
       rs.push(xs[i + 1])
     }
     return rs
