2d0ae50aed72b66d8dfee0ca0c4d5e597e88389f
Mirek Rusin <mirek@me.com>
2021-03-05 09:25:51 +0100

    Query support in where.

---
+++ test/where.test.ts
@@ -0,0 +1,51 @@
+import gt from '../gt'
+import is from '../is'
+import where from '../where'
+
+const t =
+  (a: Record<string, unknown>, b: string): void =>
+    expect(where(a).toString()).toEqual(b)
+
+test('simple', () => {
+  t({ foo: 1 }, '(foo = 1)')
+  t({ foo: { $gt: 1 } }, '(foo > 1)')
+  t({ $and: [ { foo: { $gt: 1 } }, { foo: { $lt: 10 } } ] }, '((foo > 1) and (foo < 10))')
+})
+
+test('nested is', () => {
+  t({
+    $and: [
+      { $not: { foo: { $gt: 1 } } },
+      { foo: { $not: { $lt: 10 } } },
+      { $or: [
+        { baz: 5 },
+        { baz: is(gt, 10) }
+      ] }
+    ]
+  }, '(not ((foo > 1)) and (foo = N\'{"$not":{"$lt":10}}\') and ((baz = 5) or (baz > 10)))')
+})
+
+test('stop at leaf', () => {
+  t({ foo: { $not: { $lt: 10 } } }, '(foo = N\'{"$not":{"$lt":10}}\')')
+})
+
+test('different ops', () => {
+  t({
+    $and: [
+      { a: { $in: [ 1, 2, 3 ] } },
+      { b: { $like: 'foo%' } },
+      { c: { $gte: 5 } },
+      { d: { $lte: 1 } },
+      { e: { $lt: 0 } },
+      { f: { $notIn: [ 3, 5, 7 ] } },
+      { g: { $eq: 3 } },
+      { h: { $ne: 0 } },
+      { i: { $ng: 1 } },
+      { j: { $nl: -1 } },
+      { $or: [
+        { $not: { k: 0 } },
+        { l: 1 }
+      ] }
+    ]
+  }, '((a in (1, 2, 3)) and (b like N\'foo%\') and (c >= 5) and (d <= 1) and (e < 0) and (f not in (3, 5, 7)) and (g = 3) and (h <> 0) and (i !> 1) and (j !< -1) and (not ((k = 0)) or (l = 1)))')
+})
--- where.ts
+++ where.ts
@@ -1,19 +1,100 @@
 import and from './and'
+import auto from './auto'
 import eq from './eq'
-import id from './identifier'
+import gt from './gt'
+import gte from './gte'
+import in_ from './in'
+import like from './like'
+import lt from './lt'
+import lte from './lte'
+import ne from './ne'
+import ng from './ng'
+import nl from './nl'
+import not from './not'
+import notIn from './not-in'
+import or from './or'
 import S from './sanitised'
-import true_ from './true'
+
+const unary = {
+  $not: not
+}
+
+const binary = {
+  $eq: eq,
+  $gt: gt,
+  $gte: gte,
+  $in: in_,
+  $like: like,
+  $lt: lt,
+  $lte: lte,
+  $ne: ne,
+  $ng: ng,
+  $nl: nl,
+  $notIn: notIn
+}
+
+const logical = {
+  $and: and,
+  $or: or
+}
+
+const object =
+  (value: unknown): value is Record<string, unknown> =>
+    typeof value === 'object' && value !== null && !Array.isArray(value)
+
+/** @returns single key in object iff object has single key, `undefined` otherwise. */
+const single =
+  (value: unknown): undefined | string => {
+    if (!object(value)) {
+      return undefined
+    }
+    let result: undefined | string
+    for (const key in value) {
+      if (result === undefined) {
+        result = key
+        continue
+      }
+      return undefined
+    }
+    return result
+  }
+
+const visitEntry =
+  (key: string, value: unknown): undefined | S => {
+    if (typeof value === 'function') {
+      return value(key)
+    }
+    const key_ = single(value)
+    if (key_ && key_ in binary) {
+      return binary[key_](key, (value as Record<string, unknown>)[key_])
+    }
+    return eq(key, value)
+  }
+
+const visit =
+  (value: unknown): S => {
+    const key = single(value)
+    if (key) {
+      const value_ = (value as Record<string, unknown>)[key]
+      if (key in logical && Array.isArray(value_)) {
+        return logical[key](...value_.map(visit))
+      }
+      if (key in unary) {
+        return unary[key](visit(value_))
+      }
+    }
+    if (object(value)) {
+      const entries = Object.entries(value).map(_ => visitEntry(..._))
+      return and(...entries)
+    }
+    return auto(value)
+  }
 
 /** @returns simple where clause part from object. */
 export const where =
-  (kv: S | Record<string, unknown>): S => {
-    if (kv instanceof S) {
-      return kv
-    }
-    const vs = Object.entries(kv)
-      .map(([ k, v ]) => typeof v === 'function' ? v(id(k)) : eq(id(k), v))
-      .filter(_ => typeof _ !== 'undefined')
-    return vs.length ? and(...vs) : true_
-  }
+  (value: S | Record<string, unknown>): S =>
+    value instanceof S ?
+      value :
+      visit(value)
 
 export default where
