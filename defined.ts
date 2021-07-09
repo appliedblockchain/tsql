ffab09d0152690e13d3b1edc92a535eedd220f31
Mirek Rusin <mirek@me.com>
2021-07-09 15:24:41 +0200

    Omit undefined in insert-object.

---
+++ defined.ts
@@ -0,0 +1,13 @@
+/** @returns object without undefined values. */
+const defined =
+  (object: Record<string, unknown>): Record<string, unknown> => {
+    const object_ = Object.assign({}, object)
+    for (const key in object) {
+      if (typeof object_[key] === 'undefined') {
+        delete object_[key]
+      }
+    }
+    return object_
+  }
+
+export default defined
--- insert-object.ts
+++ insert-object.ts
@@ -1,21 +1,23 @@
 import { inspect } from 'util'
 import auto from './auto'
+import defined from './defined'
 import id from './identifier'
 import list from './list'
+import tsql from './template'
 import type S from './sanitised'
 import type Sid from './sanitised-identifier'
-import tsql from './template'
 
 /** @returns insert `object` into `table` statement. */
 export const insertObject =
   (table: Sid | string, object: Record<string, unknown>): S => {
-    const keys = Object.keys(object)
+    const object_ = defined(object)
+    const keys = Object.keys(object_)
     if (!keys.length) {
-      throw new TypeError(`Expected object with keys, got ${inspect(object)}.`)
+      throw new TypeError(`Expected object with keys, got ${inspect(object_)}.`)
     }
     const table_ = id(table)
     const keys_ = list(keys.map(id))
-    const values_ = list(keys.map(_ => object[_]).map(auto))
+    const values_ = list(keys.map(_ => object_[_]).map(auto))
     return tsql`insert into ${table_} (${keys_}) values (${values_})`
   }
 
