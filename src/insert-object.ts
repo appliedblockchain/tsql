b70a7d9348aeb54f09f2e32492e99626c6d00e43
Mirek Rusin <mirek@me.com>
2023-01-30 12:09:52 +0100

    feat: support output for insert-object

--- src/insert-object.ts
+++ src/insert-object.ts
@@ -14,7 +14,7 @@ import type Sid from './sanitised-identifier.js'
  * @throws {TypeError} if there are no non-`undefined` entries.
  */
 export const insertObject =
-  (table: Sid | string, object: Record<string, unknown>): S => {
+  (table: Sid | string, object: Record<string, unknown>, output?: S): S => {
     const object_ = defined(object)
     const keys = Object.keys(object_)
     if (!keys.length) {
@@ -23,7 +23,9 @@ export const insertObject =
     const table_ = id(table)
     const keys_ = list(keys.map(id))
     const values_ = list(keys.map(_ => object_[_]).map(auto))
-    return tsql`insert into ${table_} (${keys_}) values (${values_})`
+    return output ?
+      tsql`insert into ${table_} (${keys_}) ${output} values (${values_})` :
+      tsql`insert into ${table_} (${keys_}) values (${values_})`
   }
 
 export default insertObject
