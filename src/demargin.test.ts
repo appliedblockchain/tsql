ae5b398d4fe41ee41391f00d90f1a8dd291222e7
Mirek Rusin <mirek@me.com>
2023-04-25 09:13:53 +0200

    test(demargin): adding demargin tests

---
+++ src/demargin.test.ts
@@ -0,0 +1,70 @@
+import * as Tsql from './index.js'
+
+test('simple', () => {
+  expect(Tsql.demargin(`
+    foo
+    bar
+    baz
+  `)).toEqual([
+    'foo',
+    'bar',
+    'baz'
+  ].join('\n'))
+})
+
+test('parens', () => {
+  expect(Tsql.demargin(`
+    foo = (
+      bar
+    )
+  `)).toEqual([
+    'foo = (',
+    '  bar',
+    ')'
+  ].join('\n'))
+  expect(Tsql.demargin(`
+    (
+      foo
+    )
+  `)).toEqual([
+    '(',
+    '  foo',
+    ')'
+  ].join('\n'))
+})
+
+test('nested modify_json', () => {
+  expect(Tsql.demargin(`
+    fooJson = (
+      json_modify(
+        json_modify(
+          json_modify(
+            fooJson,
+            N'$.foo',
+            N'FOO'
+          ),
+          N'$.bar',
+          2
+        ),
+        N'$.baz',
+        cast(1 as bit)
+      )
+    )
+  `)).toEqual([
+    'fooJson = (',
+    '  json_modify(',
+    '    json_modify(',
+    '      json_modify(',
+    '        fooJson,',
+    '        N\'$.foo\',',
+    '        N\'FOO\'',
+    '      ),',
+    '      N\'$.bar\',',
+    '      2',
+    '    ),',
+    '    N\'$.baz\',',
+    '    cast(1 as bit)',
+    '  )',
+    ')'
+  ].join('\n'))
+})
